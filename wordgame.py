3fac7ba0725f39875e044948f65f3d05c36cbb8e
yakuri354 <yakuri2006@gmail.com>
2019-08-22 12:36:05 +0300

    Thu, 1st lesson, tried to add typing, not complete

--- wordgame.py
+++ wordgame.py
@@ -7,6 +7,7 @@ H = 400
 WHITE = (255, 255, 255)
 LBLUE = (122, 184, 225)
 
+
 pygame.init()
 pygame.font.init()
 sc = pygame.display.set_mode((W, H))
@@ -19,6 +20,14 @@ r = 46
 inf = True
 words = ['fine', 'test', 'good', 'nope', 'maze']
 circs = []
+close = 0
+lastwords = []
+func = 0
+letter = None
+keypressed = 'a'
+lastletter = 1
+pressed = False
+
 
 
 class Circword:
@@ -32,45 +41,77 @@ class Circword:
         self.x = int(self.x)
 
     def draw(self, sc):
-        print(self)
         pygame.draw.circle(sc, LBLUE, (self.x, self.y), r)
         pygame.font.SysFont('arial', 20)
         font = pygame.font.Font(None, 72)
         text = font.render(self.word, 1, (0, 100, 0))
         place = text.get_rect(center=(self.x, self.y))
+        lastwords.append(self.word)
         sc.blit(text, place)
 
 
 circ = Circword(random.choice(words))
+
 
 while inf:
     sc.fill(WHITE)
     circ.draw(sc)
     if timetonew < 0:
         circs.append(Circword(random.choice(words)))
-        timetonew = 1000
-    # pygame.draw.circle(sc, LBLUE, (x1, y1), r)
-    # pygame.draw.circle(sc, LBLUE, (x1, y2), r)
-    #x1 -= 1
+        timetonew = 6000
+        letter = 0
     delta = clock.tick(FPS)
     circ.update(delta)
     timetonew -= delta
     for c in circs:
         c.update(delta)
         c.draw(sc)
-    circs = filter(lambda c: not c.died, circs)
+    circls = filter(lambda c: not c.died, circs)
     pygame.display.update()
+    lastword = lastwords[0]
+    letter = lastword[lastletter]
+    if letter == None:
+        letter = 'a'
+    print('letter', letter)
+    print('lastletter', lastletter)
+    print('lastword', lastword)
+    if keypressed == letter:
+        if lastletter < 4:
+            lastletter += 1
+            print('You pressed the right key!')
+        else:
+            lastletter = 0
+            lastwords.remove(0)
+            circs.remove(0)
+            print('The word is complete! Next word is', lastwords[0])
+    # else:
+    #     print('nah')
+    #     pressed = False
+
+
 
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
-    #     elif i.type == pygame.KEYDOWN:
-    #         if i.key == pygame.K_LEFT:
-    #             x -= 3
-    #         elif i.key == pygame.K_RIGHT:
-    #             x += 3
 
-font = pygame.font.SysFont('arial', 100)
-text = font.render('Game Over', 1, (0, 100, 0))
-place = text.get_rect(center=(W / 2, H / 2))
-sc.blit(text, place)
+
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_ESCAPE:
+                inf = False
+            else:
+                keypressed = i.unicode
+                pressed = True
+
+
+while close < 2000:
+    pygame.display.update()
+    sc.fill(WHITE)
+    font = pygame.font.SysFont('arial', 100)
+    text = font.render('Game Over', 1, (0, 100, 0))
+    place = text.get_rect(center=(W / 2, H / 2))
+    sc.blit(text, place)
+    pygame.time.delay(1)
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            exit()
+    close += 1
